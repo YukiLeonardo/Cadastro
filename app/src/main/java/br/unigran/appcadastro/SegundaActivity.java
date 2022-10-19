ac54268e2f9ee244443c3b21a6ffc84f83480e78
Leonardo <802.293@alunos.unigran.br>
2022-10-18 22:51:34 -0300

    Fragmentos e Segunda tela

--- app/src/main/java/br/unigran/appcadastro/SegundaActivity.java
+++ app/src/main/java/br/unigran/appcadastro/SegundaActivity.java
@@ -1,14 +1,81 @@
 package br.unigran.appcadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SegundaActivity extends AppCompatActivity {
+    Button cadastrar;
+    Button listar;
+    Button voltar;
+
+    Integer Operacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        cadastrar = findViewById(R.id.id_cadastro);
+        listar = findViewById(R.id.id_listagem);
+        voltar = findViewById(R.id.id_voltar);
+        Operacao = getIntent().getIntExtra("operacao", -1);
+
+
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (Operacao){
+        case -1:{
+            onBackPressed();
+            break;
+        }
+
+        case 0:{
+            transaction.add(R.id.fragmentoPrincipal, );
+            break;
+        }
+
+        case 1:{
+            transaction.add(R.id.fragmentoPrincipal, );
+            break;
+        }
+
+        case 2:{
+            transaction.add(R.id.fragmentoPrincipal, );
+            break;
+        }
     }
+
+        transaction.commit();
+    acoesComponentes();
+}
+
+    private void acoesComponentes() {
+        listar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (Operacao){
+                    case 0:{
+                        break;
+                    }
+                    case 1:{
+                        break;
+                    }
+                    case 2:{
+                        break;
+                    }
+                }
+            }
+        });
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 }
