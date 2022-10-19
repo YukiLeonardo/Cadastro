792f48cdc69ed10f4a522ced25653be0b9fdf9d0
Leonardo <802.293@alunos.unigran.br>
2022-10-18 22:37:07 -0300

    Segunda Tela e transição

--- app/src/main/java/br/unigran/appcadastro/MainActivity.java
+++ app/src/main/java/br/unigran/appcadastro/MainActivity.java
@@ -2,13 +2,69 @@ package br.unigran.appcadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+        Button cliente;
+        Button produto;
+        Button fornecedor;
+        Button sair;
+        Intent intent;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            cliente = findViewById(R.id.id_cliente);
+            produto = findViewById(R.id.id_produto);
+            fornecedor = findViewById(R.id.id_fornecedor);
+            sair = findViewById(R.id.id_sair);
+
+            acoesComponentes();
+    }
+
+
+    private void acoesComponentes() {
+        produto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("operacao", 0);
+
+                startActivity(intent);
+            }
+        });
+
+        fornecedor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("operacao", 1);
+
+                startActivity(intent);
+            }
+        });
+
+        cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("operacao", 2);
+
+                startActivity(intent);
+            }
+        });
+
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
 }
+
